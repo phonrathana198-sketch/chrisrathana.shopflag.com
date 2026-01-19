424611dc9ded70b89e11655d6ec6030772b2957e
cydersnake.eyes ng <phonrathana198@gmail.com>
2026-01-20 01:51:09 +0700

    Update ssl_check.py

--- git/ssl_check.py
+++ git/ssl_check.py
@@ -2,7 +2,7 @@ import ssl
 import socket
 from datetime import datetime
 
-HOST = "www.smailshop.stoer.com"
+HOST = "www.chrisrathana.shopflag.com"
 PORT = 443
 
 print("Checking SSL for:", HOST)
@@ -11,13 +11,13 @@ print("-" * 40)
 try:
     context = ssl.create_default_context()
     with socket.create_connection((HOST, PORT), timeout=10) as sock:
-        with context.wrap_socket(sock, server_hostname=HOST) as ssock:
-            cert = ssock.getpeercert()
-            print("SSL STATUS: OK ✅")
-            print("Issued To:", cert.get("subject"))
-            print("Issuer:", cert.get("issuer"))
-            print("Valid From:", cert.get("notBefore"))
-            print("Valid Until:", cert.get("notAfter"))
+       with context.wrap_socket(sock, server_hostname=HOST) as ssock:
+       cert = ssock.getpeercert()
+print("SSL STATUS: OK ✅")
+print("Issued To:", cert.get("subject"))
+print("Issuer:", cert.get("issuer"))
+print("Valid From:", cert.get("notBefore"))
+print("Valid Until:", cert.get("notAfter"))
 except Exception as e:
-    print("SSL STATUS: FAILED ❌")
-    print("Reason:", e)
+print("SSL STATUS: FAILED ❌")
+print("Reason:", e)
